fae0e9746c11270057ad4e3caf7d74aceeaf3ff8
MarcoJurado20308 <71940438+MarcoJurado20308@users.noreply.github.com>
2021-02-13 17:34:03 -0600

    Comentarios, Gnome, Merge

--- Main.java
+++ Main.java
@@ -1,20 +1,33 @@
+/*******************************************************
+* Universidad del Valle de Guatemala
+* Algoritmos y Estructuras de Datos
+* Profesor: Moises Gonzales
+* Main.java 
+* Autor: Stefano Aragoni 20261, Marco Jurado 20308 
+*
+********************************************************/
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
-
 import java.util.Scanner; 
 
 class Main {
   public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in); 
+    Scanner scanner = new Scanner(System.in); //Se crea el scanner para recibir ingresos del usuario 
 
     System.out.println("--SORTS--\n"); 
 
     System.out.println("Cuantos numeros quieres que el archivo tenga?"); 
 
+    /**
+     * Se le realiza la pregunta al usuario sobre que cantidad
+     * de datos quiere que el archivo contenga para luego estos
+     * ser ordenados por alguno de los metodos de sorting. 
+     */
     int cantTotal = scanner.nextInt(); 
 
     int opcion1 = 0;
@@ -23,17 +36,31 @@ class Main {
 
     File file = null;
 
-    Integer[] database = new Integer[total];; 
+    /**
+     * Se crea la base de datos con el tipo de dato Integer[]
+     * pues este tipo de dato aplica la interfaz Compare[]. 
+     * Por lo tanto se pueden utilizar los métodos de la interfaz
+     * y se permite la conversion de los datos tipo integer para
+     * facilitar el funcionamiento. 
+     */
+    Integer[] database = new Integer[total];;  
 
     boolean test = false;
 
     while(!test){
 
+      /**
+       * Se abre un ciclo while en donde se le pregunta al usuario que tipo de orden 
+       * quiere que tengan los datos del archivo de texto. Estos pueden ser:
+       * 1. Ordenados
+       * 2. Desordenados 
+       */
+
       System.out.println("\nQuieres crear un .txt con  numeros ordenados o desordenados?\n1. Desordenados \n2. Ordenados");
 
-      opcion1 = scanner.nextInt(); 
+      opcion1 = scanner.nextInt(); //El usuario ingresa su seleccion
 
-      if(opcion1 == 1){
+      if(opcion1 == 1){ //Opcion Desordenados
         try {
           BufferedWriter output = null;
 
@@ -47,6 +74,9 @@ class Main {
           Random random = new Random();
 
           while (total > 0) {
+            /**
+             * En un ciclo se crean todos los números que se encontrarán en el archivo de texto. 
+             */
 
             line = random.nextInt(1000);
             database[tempLine2] = Integer.valueOf(line);
@@ -63,7 +93,7 @@ class Main {
         }
         
 
-      }else if (opcion1 == 2){
+      }else if (opcion1 == 2){ //Opcion Ordenados
         try {
           BufferedWriter output = null;
 
@@ -73,6 +103,11 @@ class Main {
           int line = 0;
 
           while (total > 0) {
+
+            /**
+             * En un ciclo se crean todos los números que se encontrarán en el archivo de texto.
+             * A diferencia del pasado este los genera ordenadamente.  
+             */
 
             database[line] = Integer.valueOf(line);
             output.write(new Integer(line).toString());
@@ -93,32 +128,47 @@ class Main {
     }
 
     
+
+
+    /**
+     * Ahora que los archivos fueron creados, se le pregunta al usuario que tipo de sort desea
+     * utilizar para ordenar el contenido númerico de estos mismos. Se le mostrarán los sorts de:
+     * 1. Bubble Sort
+     * 2. Gnome Sort
+     * 3. Merge Sort
+     * 4. Quick Sort
+     * 5. Radix Sort
+     */
     int opcion = 0;
 
     while(opcion != 6){
 
       System.out.println("\nCon qué sort quieres ordenar los números?\n1. Bubble Sort\n2. Gnome Sort\n3. Merge Sort\n4. Quick Sort\n5. Radix Sort\n6. SALIR");
 
-      opcion = scanner.nextInt(); 
+      opcion = scanner.nextInt(); //Ingreso del usuario 
       Sort sortMethods = new Sort();
       Integer[] tComparable = new Integer[cantTotal];
 
-      if(opcion == 1){
+      if(opcion == 1){ //Bubble
         tComparable = sortMethods.bubbleSort(database);
-      }else if(opcion == 2){
+      }else if(opcion == 2){ //Gnome
 
-      }else if(opcion == 3){
+      }else if(opcion == 3){ //Merge
 
-      }else if(opcion == 4){
+      }else if(opcion == 4){ //Quick
 
-      }else if(opcion == 5){
+      }else if(opcion == 5){ //Radix
 
-      }else if(opcion == 6){
+      }else if(opcion == 6){ //Salir
         System.out.println("Adios."); 
         System.exit(0); 
-
       }
 
+
+      /**
+       * Una vez ejecutados los metodos de cada tipo de sort se generan los nuevos archivos 
+       * con los datos en la base de datos "database". Este es el archivo final. 
+       */
       try {
         
         BufferedWriter output = null;
--- Sort.java
+++ Sort.java
@@ -1,9 +1,23 @@
+/*******************************************************
+* Universidad del Valle de Guatemala
+* Algoritmos y Estructuras de Datos
+* Profesor: Moises Gonzales
+* Sort.java
+* Autor: Stefano Aragoni 20261, Marco Jurado 20308 
+*
+********************************************************/
+
 class Sort{
 
   public Sort(){
 
   }
 
+  /**
+   * @author Stefano Aragoni
+   * @param database
+   * @return Integer[] Listado ordenado
+   */
   public Integer[] bubbleSort(Integer[] database){ 
 
     for (int i = 0; i < database.length; i++){
@@ -21,18 +35,122 @@ class Sort{
     return database;
   } 
 
-  public void GnomeSort(){
+
+  /**
+   * @author Marco Jurado 
+   * @param database
+   * @return Integer[] Listado ordenado
+   */
+  public Integer[] GnomeSort(Integer[] database){
+
+    int index = 0; //Se define un index para ubicarse en el listado
+
+    while(index < database.length){
+      if(index == 0){
+        //El index es 0, avanza a siguiente elemento
+        index++;
+      }
+      if(database[index].compareTo(database[index - 1]) >= database[index - 1] ){
+        //El elemento en el cual se encuentra el index es mayor o igual que el anterior. Continua
+        index++;
+      }
+      else{
+        //El elemento en el que se encuentra debe de ser ordenado
+        int variable_temporal = 0; //Variable que sirve para guardar la posicion que se evalua
+
+        variable_temporal = database[index];
+        database[index] = database[index - 1]; // se mueve el elemento pues a la posicion menor
+        database[index - 1] = variable_temporal; // Se pone el elemento de la posicion anterior en la siguiente, pues es mayor. 
+        index--;
+
+      }
+    }
+    
+    /**
+     * Una vez se encuentran ordenados los elementos se procede a devolver el arreglo. 
+     */
+
+    return database;
 
   }
 
-  public void MergeSort(){
+  
+  /**
+   * @author Marco Jurado 
+   * @param database
+   * @return Integer[] Listado ordenado
+   */
+  public Integer[] MergeSort(Integer[] database){
 
-  }
+    /**
+     * Cabe mencionar que este tipo de sort es recursivo.
+     */
 
+    int medio = database.length / 2;
+
+    /**Se crearan dos arreglos los cuales serviran para 
+     * que se dividan los datos y se puedan realizar las comparaciones en ambos lados.
+     */
+    
+    Integer[] Izquierda = new Integer[medio];
+    Integer[] Derecha = new Integer[database.length - medio];
+
+    for(int i = 0; i < medio; i++){
+      //Se especifican que las posiciones en el arrgelo son las mismas que en la base de datos. 
+      Izquierda[i] = database[i];
+    }
+    for(int i = 0; i < medio; i++){
+      //Se especifican que las posiciones en el arrgelo son las mismas que en la base de datos. 
+      Derecha[i - medio] = database[i];
+    }
+
+    MergeSort(Izquierda);
+    MergeSort(Derecha);
+
+    //Ahora se juntan todas las piezas
+
+    int a = 0;
+    int b = 0;
+    int c = 0;
+
+    while(a < Izquierda.length && b < Derecha.length){
+      if(Izquierda[a].compareTo(Derecha[b]) <= Derecha[b]){
+        database[c++] = Izquierda[a++];
+      }
+      else {
+        database[c++] = Derecha[b++];
+      }
+      }
+      while (a < Izquierda.length) {
+        database[c++] = Izquierda[a++];
+      }
+      while (b < Derecha.length) {
+        database[c++] = Derecha[b++];
+      }
+
+      /**
+       * Ahora que todas las piezas estan juntas se devuelve la base de datos ordenada
+       */
+
+       return database;
+    }
+
+  
+  /**
+   * @author Marco Jurado 
+   * @param database
+   * @return Integer[] Listado ordenado
+   */
   public void QuickSort(){
 
   }
 
+  
+  /**
+   * @author Marco Jurado 
+   * @param database
+   * @return Integer[] Listado ordenado
+   */
   public void RadixSort(){
 
   }
